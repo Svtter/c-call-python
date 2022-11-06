52759975b686839c6019aaf2d7cfc83b9f7b0c3e
svtter <svtter@qq.com>
2022-11-06 20:43:54 +0800

    feat: add python lib support!

---
+++ README.md
@@ -0,0 +1,3 @@
+# C call python
+
+1. [CMake](https://zhuanlan.zhihu.com/p/500002865)
---
+++ main.c
@@ -0,0 +1,67 @@
+#include "stdio.h"
+#include <Python.h>
+
+/* Execute func(x,y) in the Python interpreter. The
+    arguments and return result of the function must be Python floats */
+double call_func(PyObject *func, double x, double y)
+{
+
+    PyObject *args;
+    PyObject *kwargs;
+    PyObject *result = 0;
+    double retval;
+
+    /* Make sure we own the GIL */
+    PyGILState_STATE state = PyGILState_Ensure();
+
+    /* Verify that func is a proper callable */
+    if (!PyCallable_Check(func))
+    {
+
+        fprintf(stderr, "call_func: expected a callablen");
+        goto fail;
+    }
+    /* Build arguments */
+    args = Py_BuildValue("(dd)", x, y);
+    kwargs = NULL;
+
+    /* Call the function */
+    result = PyObject_Call(func, args, kwargs);
+    Py_DECREF(args);
+    Py_XDECREF(kwargs);
+
+    /* Check for Python exceptions (if any) */
+    if (PyErr_Occurred())
+    {
+
+        PyErr_Print();
+        goto fail;
+    }
+
+    /* Verify the result is a float object */
+    if (!PyFloat_Check(result))
+    {
+
+        fprintf(stderr, "call_func : callable didn’t return a floatn");
+        goto fail;
+    }
+
+    /* Create the return value */
+    retval = PyFloat_AsDouble(result);
+    Py_DECREF(result);
+
+    /* Restore previous GIL state and return */
+    PyGILState_Release(state);
+    return retval;
+
+fail:
+    Py_XDECREF(result);
+    PyGILState_Release(state);
+    abort(); // Change to something more appropriate
+}
+
+int main()
+{
+    printf("Hello World!\n");
+    return 0;
+}
